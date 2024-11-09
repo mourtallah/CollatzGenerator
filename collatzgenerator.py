e7b0e165e59ac8dce75434429c2a493697b628cc
Mourtallah Faye <serigne.faye@icloud.com>
2024-11-09 22:27:03 +0000

    refactor code for clarity; add file header

--- collatzgenerator.py
+++ collatzgenerator.py
@@ -1,36 +1,52 @@
-import matplotlib.pyplot as plt
-import random
+"""
+Collatz Sequence Generator
 
-print('Welcome to the Collatz Sequence Generator!')
-print('Select a number:')
-n = input()
-q = n
-seq=[]
-tst = 1
-if n==1:
-    print('q')
-else:
-    while int(n) != 1:
-        seq.append(int(n))
-        if int(n) %2 == 0:
-            n = int(n)/2
-        else:
-            n=3*int(n) +1
-        tst = tst + 1
-if len(seq) > 20:
-    print("A sample from the Collatz Sequence for the number " + str(q) + " is:")
-    print ([ seq[i] for i in sorted(random.sample(range(len(seq)), 20)) ], end = "")
+This script generates the Collatz sequence for a given number.
+The Collatz sequence is defined as follows:
+- Start with any positive integer n.
+- Then each term is obtained from the previous term as follows:
+  - If the previous term is even, the next term is one half of the previous term.
+  - If the previous term is odd, the next term is 3 times the previous term plus 1.
+The sequence ends when it reaches the number 1.
+
+Author: Mourtallah Faye
+Date: 2023-04-30
+"""
+import random
+import matplotlib.pyplot as plt
+
+# User input
+user_input = int(input("Enter a number: "))
+starting_number = user_input
+
+# Initialize sequence and stopping time
+collatz_sequence = []
+stopping_time = 1
+
+# Generate sequence
+while starting_number != 1:
+    collatz_sequence.append(int(starting_number))
+    if starting_number % 2 == 0:
+        starting_number = starting_number/2
+    else:
+        starting_number = 3*starting_number + 1
+    stopping_time += 1
+
+# Print results
+if len(collatz_sequence) > 20:
+    print("A sample of 20 elements from the Collatz Sequence for the number " +
+          str(user_input) + " is:")
+    print([int(collatz_sequence[i]) for i in sorted(random.sample(range(len(collatz_sequence)), 20))], end="")
     print(",")
 else:
-    print("The Collatz Sequence for the number " + str(q) + " is:")
-    print(seq, end = "")
+    print("The Collatz Sequence for the number " + str(user_input) + " is:")
+    print(collatz_sequence, end="")
     print(",")
-print("with a total stopping time of " + str(tst) + ",")
-if q == 1:
-    print("T")
+print("This sequence has a total stopping time of " + str(stopping_time) + ".")
+if user_input == 1:
+    print("The number 1 has no Collatz Sequence, so the peak of the sequence is 1.")
 else:
-    print("the peak of the sequence being " + str(max(seq))+".")
-plt.plot(seq)
+    print("The peak of the sequence is " + str(max(collatz_sequence)) + ".")
+plt.plot(collatz_sequence)
 plt.show()
 
-# git test 
